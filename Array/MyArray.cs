4cf4ae9449df471f64f58f81238a0c4e0b6fc9f9
haUk2 <91018531+haUk2@users.noreply.github.com>
2023-01-20 15:40:02 +0100

    commit 20-01-2023

---
+++ Array/MyArray.cs
@@ -0,0 +1,72 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    public interface MyArray
+    {
+        
+        void SetSortStrategy(SortStrategy sortStrategy);
+        void Sort(int[] array, int start, int end);
+        void SortDesc(int[] array, int start, int end);
+
+
+    }
+
+    public abstract class SortStrategy
+    {
+        public abstract void Sort(int[] array, int start, int end);
+        public abstract void SortDesc(int[] array, int start, int end);
+
+    }
+
+  
+    public class QuickSort : SortStrategy
+    {
+        public override void Sort(int[] array, int start, int end)
+        {
+            var i = start;
+            var j = end;
+            var pivot = array[start];
+
+            while (i <= j)
+            {
+                while (array[i] < pivot)
+                {
+                    i++;
+                }
+
+                while (array[j] > pivot)
+                {
+                    j--;
+                }
+
+                if (i <= j)
+                {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+
+            if (start < j)
+                Sort(array, start, j);
+
+            if (i < end)
+                Sort(array, i, end);
+
+            
+        }
+
+        public override void SortDesc(int[] arr, int start, int end)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Array/array.cs
@@ -0,0 +1,39 @@
+﻿
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Array
+{
+    public class array
+    {
+        public array()
+        {
+            int[] array = null;
+        }
+
+        SortStrategy sortstrategy;
+        
+        public void SetSortStrategy(SortStrategy sortStrategy)
+        {
+            sortstrategy = sortStrategy;
+        }
+
+       
+        public void Sort()
+        {
+            
+        }
+
+        public void SortDesc()
+        {
+            
+        }
+    }
+}
